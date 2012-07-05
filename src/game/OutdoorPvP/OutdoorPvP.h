a843e8fe73dcef4348ae662192891b4ca5f0cbd4
Schmoozerd <schmoozerd@scriptdev2.com>
2012-07-06 00:31:50 +0200

    Add OutdoorPvP class TODO !!
    TODO: Remove all these includes in the header, and use forward declarations

--- src/game/OutdoorPvP/OutdoorPvP.h
+++ src/game/OutdoorPvP/OutdoorPvP.h
@@ -19,4 +19,91 @@
 #ifndef OUTDOOR_PVP_H
 #define OUTDOOR_PVP_H
 
+#include "../Language.h"
+#include "../World.h"
+#include "../ZoneScript.h"
+#include "../Player.h"
+#include "Common.h"
+#include "Policies/Singleton.h"
+#include "../SharedDefines.h"
+#include "../GameObject.h"
+#include "../ObjectMgr.h"
+
+enum CapturePointArtKits
+{
+    CAPTURE_ARTKIT_ALLIANCE = 2,
+    CAPTURE_ARTKIT_HORDE    = 1,
+    CAPTURE_ARTKIT_NEUTRAL  = 21
+};
+
+enum CapturePointAnimations
+{
+    CAPTURE_ANIM_ALLIANCE   = 1,
+    CAPTURE_ANIM_HORDE      = 0,
+    CAPTURE_ANIM_NEUTRAL    = 2
+};
+
+typedef std::set<Player*> PlayerSet;
+
+class OutdoorPvP : public ZoneScript
+{
+    friend class OutdoorPvPMgr;
+
+    public:
+        OutdoorPvP() {}
+        ~OutdoorPvP() {}
+
+        // called when the zone is initialized
+        virtual void FillInitialWorldStates(WorldPacket& /*data*/, uint32& /*count*/) {}
+
+        // called when a player triggers an areatrigger
+        virtual bool HandleAreaTrigger(Player* /*player*/, uint32 /*triggerId*/) { return false; }
+
+        // called when a playerd drops a flag
+        virtual bool HandleDropFlag(Player* /*player*/, uint32 /*spellId*/) { return false; }
+
+        // called when a playerd uses a gameobject related to world pvp events
+        virtual bool HandleObjectUse(Player* /*player*/, GameObject* /*go*/) { return false; }
+
+        // handle npc/player kill
+        virtual void HandlePlayerKillInsideArea(Player* /*killer*/, Unit* /*victim*/) {}
+        virtual void HandlePlayerKill(Player* killer, Unit* victim);
+
+        // handle capture objective complete
+        virtual void HandleObjectiveComplete(uint32 /*eventId*/, std::list<Player*> /*players*/, Team /*team*/) {}
+
+        // init all the outdoor pvp area relates stuff
+        virtual bool InitOutdoorPvPArea() { return false; }
+
+        // send world state update to all players present
+        void SendUpdateWorldState(uint32 field, uint32 value);
+
+        // update - called by the OutdoorPvPMgr
+        virtual void Update(uint32 diff) {}
+
+        // applies buff to a team inside the specific zone
+        void BuffTeam(Team team, uint32 spellId, bool remove = false);
+
+        // get banner artkit based on controlling team
+        uint32 GetBannerArtKit(Team team, uint32 artKitAlliance, uint32 artKitHorde, uint32 artKitNeutral);
+
+        // set banner visual
+        void SetBannerVisual(const WorldObject* objRef, ObjectGuid goGuid, uint32 artKit, uint32 animId);
+        void SetBannerVisual(GameObject* go, uint32 artKit, uint32 animId);
+
+    protected:
+        // Player related stuff
+        virtual void HandlePlayerEnterZone(Player* player);
+        virtual void HandlePlayerLeaveZone(Player* player);
+
+        // remove world states
+        virtual void SendRemoveWorldStates(Player* player) {}
+
+        void RegisterZone(uint32 zoneId);
+        void SetCapturePointSliderValue(uint32 entry, CapturePointSlider value);
+
+        // store the players inside the area depending on the team
+        PlayerSet m_zonePlayers;
+};
+
 #endif
