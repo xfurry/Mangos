4cb628b2740f51dd406449fa3a2a581993d97980
Schmoozerd <schmoozerd@scriptdev2.com>
2012-07-06 00:48:59 +0200

    Add Grizzly Hills Opvp

--- src/game/OutdoorPvP/OutdoorPvPGH.cpp
+++ src/game/OutdoorPvP/OutdoorPvPGH.cpp
@@ -15,3 +15,150 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
+
+#include "OutdoorPvP.h"
+#include "OutdoorPvPGH.h"
+
+
+OutdoorPvPGH::OutdoorPvPGH() : OutdoorPvP(),
+    m_zoneOwner(TEAM_NONE)
+{
+}
+
+bool OutdoorPvPGH::InitOutdoorPvPArea()
+{
+    RegisterZone(ZONE_ID_GRIZZLY_HILLS);
+
+    return true;
+}
+
+void OutdoorPvPGH::OnCreatureCreate(Creature* creature)
+{
+    switch (creature->GetEntry())
+    {
+        case NPC_WESTFALL_BRIGADE_DEFENDER:
+        case NPC_COMMANDER_HOWSER:
+            m_allianceSoldiers.push_back(creature->GetObjectGuid());
+            if (m_zoneOwner == ALLIANCE)
+                return;
+            break;
+        case NPC_BLACKSMITH_JASON_RIGGINS:
+        case NPC_STABLE_MASTER_TIM:
+        case NPC_VENDOR_ADAMS:
+        case NPC_HORSE:
+            // check the zone id because the horses can be found in other areas too
+            if (creature->GetZoneId() == ZONE_ID_GRIZZLY_HILLS)
+                m_allianceVendors.push_back(creature->GetObjectGuid());
+            if (m_zoneOwner == ALLIANCE)
+                return;
+            break;
+        case NPC_CONQUEST_HOLD_DEFENDER:
+        case NPC_GENERAL_GORLOK:
+            m_hordeSoldiers.push_back(creature->GetObjectGuid());
+            if (m_zoneOwner == HORDE)
+                return;
+            break;
+        case NPC_BLACKSMITH_KOLOTH:
+        case NPC_STABLE_MASTER_KOR:
+        case NPC_VENDOR_PURKOM:
+        case NPC_RIDING_WOLF:
+            // check the zone id because the wolfs can be found in other areas too
+            if (creature->GetZoneId() == ZONE_ID_GRIZZLY_HILLS)
+                m_hordeVendors.push_back(creature->GetObjectGuid());
+            if (m_zoneOwner == HORDE)
+                return;
+            break;
+
+        default:
+            return;
+    }
+
+    // Despawn creatures on create - will be spawned later in script
+    creature->SetRespawnDelay(7 * DAY);
+    creature->ForcedDespawn();
+}
+
+void OutdoorPvPGH::OnGameObjectCreate(GameObject* go)
+{
+    if (go->GetEntry() == GO_VENTURE_BAY_LIGHTHOUSE)
+        go->SetGoArtKit(GetBannerArtKit(m_zoneOwner, CAPTURE_ARTKIT_ALLIANCE, CAPTURE_ARTKIT_HORDE, CAPTURE_ARTKIT_NEUTRAL));
+}
+
+// process the capture events
+void OutdoorPvPGH::ProcessEvent(uint32 eventId, GameObject* go)
+{
+    // If we are not using the lighthouse return
+    if (go->GetEntry() != GO_VENTURE_BAY_LIGHTHOUSE)
+        return;
+
+    switch (eventId)
+    {
+        case EVENT_LIGHTHOUSE_WIN_ALLIANCE:
+            // Spawn the npcs only when the tower is fully controlled
+            m_zoneOwner = ALLIANCE;
+            RespawnSoldiers(go);
+            break;
+        case EVENT_LIGHTHOUSE_WIN_HORDE:
+            // Spawn the npcs only when the tower is fully controlled
+            m_zoneOwner = HORDE;
+            RespawnSoldiers(go);
+            break;
+        case EVENT_LIGHTHOUSE_PROGRESS_ALLIANCE:
+            SetBannerVisual(go, CAPTURE_ARTKIT_ALLIANCE, CAPTURE_ANIM_ALLIANCE);
+            break;
+        case EVENT_LIGHTHOUSE_PROGRESS_HORDE:
+            SetBannerVisual(go, CAPTURE_ARTKIT_HORDE, CAPTURE_ANIM_HORDE);
+            break;
+        case EVENT_LIGHTHOUSE_NEUTRAL_ALLIANCE:
+        case EVENT_LIGHTHOUSE_NEUTRAL_HORDE:
+            m_zoneOwner = TEAM_NONE;
+            SetBannerVisual(go, CAPTURE_ARTKIT_NEUTRAL, CAPTURE_ANIM_NEUTRAL);
+            break;
+    }
+}
+
+void OutdoorPvPGH::RespawnSoldiers(const WorldObject* objRef)
+{
+    if (m_zoneOwner == ALLIANCE)
+    {
+        // despawn all horde vendors
+        for (std::list<ObjectGuid>::const_iterator itr = m_hordeVendors.begin(); itr != m_hordeVendors.end(); ++itr)
+        {
+            if (Creature* soldier = objRef->GetMap()->GetCreature(*itr))
+                soldier->ForcedDespawn();
+        }
+
+        // spawn all alliance soldiers and vendors
+        for (std::list<ObjectGuid>::const_iterator itr = m_allianceSoldiers.begin(); itr != m_allianceSoldiers.end(); ++itr)
+        {
+            if (Creature* soldier = objRef->GetMap()->GetCreature(*itr))
+                soldier->Respawn();
+        }
+        for (std::list<ObjectGuid>::const_iterator itr = m_allianceVendors.begin(); itr != m_allianceVendors.end(); ++itr)
+        {
+            if (Creature* soldier = objRef->GetMap()->GetCreature(*itr))
+                soldier->Respawn();
+        }
+    }
+    else
+    {
+        // despawn all alliance vendors
+        for (std::list<ObjectGuid>::const_iterator itr = m_allianceVendors.begin(); itr != m_allianceVendors.end(); ++itr)
+        {
+            if (Creature* soldier = objRef->GetMap()->GetCreature(*itr))
+                soldier->ForcedDespawn();
+        }
+
+        // spawn all horde soldiers and vendors
+        for (std::list<ObjectGuid>::const_iterator itr = m_hordeSoldiers.begin(); itr != m_hordeSoldiers.end(); ++itr)
+        {
+            if (Creature* soldier = objRef->GetMap()->GetCreature(*itr))
+                soldier->Respawn();
+        }
+        for (std::list<ObjectGuid>::const_iterator itr = m_hordeVendors.begin(); itr != m_hordeVendors.end(); ++itr)
+        {
+            if (Creature* soldier = objRef->GetMap()->GetCreature(*itr))
+                soldier->Respawn();
+        }
+    }
+}
--- src/game/OutdoorPvP/OutdoorPvPGH.h
+++ src/game/OutdoorPvP/OutdoorPvPGH.h
@@ -19,4 +19,63 @@
 #ifndef WORLD_PVP_GH
 #define WORLD_PVP_GH
 
+enum
+{
+    // zone ids
+    ZONE_ID_GRIZZLY_HILLS                   = 394,
+
+    // npcs
+    // alliance
+    NPC_WESTFALL_BRIGADE_DEFENDER           = 27758,
+    NPC_COMMANDER_HOWSER                    = 27759,
+    NPC_BLACKSMITH_JASON_RIGGINS            = 29252,
+    NPC_VENDOR_ADAMS                        = 27760,
+    NPC_STABLE_MASTER_TIM                   = 29250,
+    NPC_HORSE                               = 385,
+
+    // horde
+    NPC_CONQUEST_HOLD_DEFENDER              = 27748,
+    NPC_GENERAL_GORLOK                      = 27708,
+    NPC_BLACKSMITH_KOLOTH                   = 29253,
+    NPC_VENDOR_PURKOM                       = 27730,
+    NPC_STABLE_MASTER_KOR                   = 29251,
+    NPC_RIDING_WOLF                         = 5774,
+
+    // gameobjects
+    GO_VENTURE_BAY_LIGHTHOUSE               = 189310,
+
+    // events
+    EVENT_LIGHTHOUSE_WIN_ALLIANCE           = 18035,
+    EVENT_LIGHTHOUSE_WIN_HORDE              = 18036,
+    //EVENT_LIGHTHOUSE_CONTEST_ALLIANCE     = 18037,
+    //EVENT_LIGHTHOUSE_CONTEST_HORDE        = 18038,
+    EVENT_LIGHTHOUSE_PROGRESS_ALLIANCE      = 18039,
+    EVENT_LIGHTHOUSE_PROGRESS_HORDE         = 18040,
+    EVENT_LIGHTHOUSE_NEUTRAL_ALLIANCE       = 18041,
+    EVENT_LIGHTHOUSE_NEUTRAL_HORDE          = 18042,
+};
+
+class OutdoorPvPGH : public OutdoorPvP
+{
+    public:
+        OutdoorPvPGH();
+
+        bool InitOutdoorPvPArea();
+
+        void OnCreatureCreate(Creature* creature);
+        void OnGameObjectCreate(GameObject* go);
+        void ProcessEvent(uint32 eventId, GameObject* go);
+
+    private:
+        // respawn team soldiers
+        void RespawnSoldiers(const WorldObject* objRef);
+
+        Team m_zoneOwner;
+
+        std::list<ObjectGuid> m_allianceSoldiers;
+        std::list<ObjectGuid> m_hordeSoldiers;
+        std::list<ObjectGuid> m_allianceVendors;
+        std::list<ObjectGuid> m_hordeVendors;
+};
+
 #endif
