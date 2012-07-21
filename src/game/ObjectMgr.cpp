fe6e2769161983b41e5693b9a11a34e4b0aee2dd
stfx <stfx@hotmail.de>
2012-07-21 13:27:36 +0200

    Fix merge issues

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -5975,18 +5975,6 @@ inline void CheckAndFixGOCaptureMinTime(GameObjectInfo const* goInfo, uint32 con
     const_cast<uint32&>(dataN) = 1;
 }
 
-inline void CheckAndFixGOCaptureMinTime(GameObjectInfo const* goInfo, uint32 const& dataN, uint32 N)
-{
-    if (dataN > 0)
-        return;
-
-    sLog.outErrorDb("Gameobject (Entry: %u GoType: %u) has data%d=%u but minTime field value must be > 0.",
-        goInfo->id, goInfo->type, N, dataN);
-
-    // prevent division through 0 exception
-    const_cast<uint32&>(dataN) = 1;
-}
-
 void ObjectMgr::LoadGameobjectInfo()
 {
     SQLGameObjectLoader loader;
@@ -6147,11 +6135,6 @@ void ObjectMgr::LoadGameobjectInfo()
                 if (goInfo->flagdrop.lockId)
                     CheckGOLockId(goInfo, goInfo->flagdrop.lockId, 0);
                 CheckGONoDamageImmuneId(goInfo, goInfo->flagdrop.noDamageImmune, 3);
-                break;
-            }
-            case GAMEOBJECT_TYPE_CAPTURE_POINT:             //29
-            {
-                CheckAndFixGOCaptureMinTime(goInfo, goInfo->capturePoint.minTime, 16);
                 break;
             }
             case GAMEOBJECT_TYPE_CAPTURE_POINT:             //29
